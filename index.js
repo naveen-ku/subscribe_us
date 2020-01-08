1ab8a3448fcc66fa381284e1f0ce432f6394b84e
naveen-ku <naveen2951996@gmail.com>
2020-01-09 03:39:56 +0530

    Final commit: Completing the project and understanding the concept

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
 var app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
   host: "host",
@@ -9,12 +14,22 @@ var connection = mysql.createConnection({
   database: "subscribe_us"
 });
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res) {
   var q = "SELECT COUNT(*) AS count FROM users";
-  connection.query(q, (err, results) => {
+  connection.query(q, function(err, results) {
     if (err) throw err;
-    var test = results[0].count;
-    res.send(`We have ${test} users`);
+    var count = results[0].count;
+    res.render("home", { count: count });
+  });
+});
+
+app.post("/register", (req, res) => {
+  var person = {
+    email: req.body.email
+  };
+  connection.query("INSERT INTO users SET ?", person, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
   });
 });
 
--- schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users(
-    email VARCHAR(255) PRIMARY KEY,
-    ceated_at TIMESTAMP DEFAULT NOW()
-);
-insert into users( email) values
-('katie24@yahoo.com'), ('tsunade50@gmail.com');
