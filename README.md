07674755446b0883d4ea21a41a94e134cd4a7f26
naveen-ku <naveen2951996@gmail.com>
2020-06-03 00:05:50 +0530

    Making background image render from offline source and changing readme.md file

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ A simple web app to understand how mysql & nodeJS works together.
 + nodeJS
 + mysql
 
+
+### Run the app
+To run the app type `node index.js`
--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 var connection = mysql.createConnection({
-  host: "host",
-  user: "user",
-  password: "password",
+  host: "localhost",
+  user: "root",
+  password: "root",
   database: "subscribe_us"
 });
 
 app.get("/", function(req, res) {
   var q = "SELECT COUNT(*) AS count FROM users";
   connection.query(q, function(err, results) {
-    if (err) throw err;
+    if (err) console.log(err);
     var count = results[0].count;
     res.render("home", { count: count });
   });
@@ -33,7 +33,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-var PORT = 3000;
+var PORT = 3001;
 app.listen(PORT, () => {
   console.log(`App listening on Port: ${PORT}.`);
 });
